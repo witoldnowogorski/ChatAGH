4344b259cff58f8ea130a4051e6c4d305eb8da85
Witold Nowogorski <witek.nowogorski@ibm.com>
2025-08-23 18:34:41 +0200

    web app

--- web_app/web_app/settings.py
+++ web_app/web_app/settings.py
@@ -71,8 +71,8 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "chat" / "static"]
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
